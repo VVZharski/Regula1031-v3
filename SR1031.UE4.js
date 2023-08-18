006a74b012387de0ad5b890c2b18eb2c1cc736db
VVZharski <106373839+VVZharski@users.noreply.github.com>
2023-08-18 12:02:57 +0300

    Update SR1031.UE4.js

--- SR1031.UE4.js
+++ SR1031.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('SR1031.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/VVZharski/Regula1031-v3/1dd7c772b2ded029b1d8109e48a0571677c909dc/SR1031.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
